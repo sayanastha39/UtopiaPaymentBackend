5a4c17e34d63c33e8472b7b4b5838aba5f655a3d
sayana <sayana.shrestha@veritis.com>
2020-03-05 16:52:14 -0500

    Payment Backend

---
+++ dao/db.js
@@ -0,0 +1,20 @@
+var mysql      = require('mysql');
+const CONFIG = require('../config');
+
+var connection = mysql.createConnection({
+    host     : CONFIG.db_host,
+    port     : CONFIG.db_port,
+    user     : CONFIG.db_user,
+    password : CONFIG.db_password,
+    database : CONFIG.db_name
+});
+
+connection.connect(function(err){
+if(!err) {
+    console.log("AWS Database is connected ... ");    
+} else {
+    console.log("Error connecting AWS database ... ");    
+}
+});
+
+module.exports = connection;
---
+++ main.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+const apiRouter = require('./controllers/index.js');
+const apiRouterTest = require('./test/index.js');
+const errorHandler = require('./helpers/errorHandler');
+
+const server = express();
+server.use(bodyParser.json()); 
+server.use(bodyParser.urlencoded({ extended: true })); 
+server.use(cors());
+// server.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+//server.use(require('./controllers/paymentController.js'));
+const configureRoutes = require("./routes")
+configureRoutes(server);
+
+
+server.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the Payment Service on AWS Lambda!' });
+ });
+
+server.use('/payment', apiRouter);
+server.use('/paymentTest', apiRouterTest);
+
+server.use(errorHandler.notFound);
+server.use(errorHandler.internalServerError);
+
+module.exports = server;
+
+//ERROR CHECK : serverless logs -f server -t
+
+server.listen(8000);
+console.log('Server running in port: 8000 ...')
